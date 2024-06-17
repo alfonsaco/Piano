8ae2c4331670753d6257bac70fc2ed15bde06377
alfonsaco <alfonso.rincondev@gmail.com>
2024-06-17 14:51:38 +0200

    Ahora el botón para mostrar y ocultar las letras funciona a la perfección

--- Scripts/botones.js
+++ Scripts/botones.js
@@ -1,9 +1,10 @@
 const botonGuia=document.getElementById("boton-guia");
-const elementosAfterBefore=document.querySelectorAll(".teclas-blancas div span, .teclas-negras div div");
+const elementosAfterBefore=document.querySelectorAll(".teclas-blancas div span, .teclas-negras div div span");
 console.log(elementosAfterBefore);
 
-botonGuia.addEventListener("click", function()  {
-    elementosAfterBefore.forEach((elemento)=> {
+// Función para mostrar y ocultar los nombres de las teclas
+elementosAfterBefore.forEach((elemento) => {
+    botonGuia.addEventListener("click", function() {
         elemento.classList.toggle("mostrar-teclas");
     })
 });
